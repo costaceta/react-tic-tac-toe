b8a0ac185c66ff0468ea76e1432befc40c0d1360
Rafael Costa <leega_francisco.costa@slmandic.edu.br>
2024-12-11 12:08:55 -0300

    feat: Initial commit

---
+++ src/components/GameBoad.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+
+const GameBoad = ({ onSelectSquare, board }) => {
+  
+
+  //   const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  //   const handleSelectSquare = (rowIndex, colIndex) => {
+  //     setGameBoard((prevGameBoard) => {
+  //       const updatedGameBoard = [
+  //         ...prevGameBoard.map((innerArray) => [...innerArray]),
+  //       ];
+  //       updatedGameBoard[rowIndex][colIndex] = activePlayerSymbol;
+
+  //       return updatedGameBoard;
+  //     });
+
+  //     onSelectSquare();
+  //   };
+
+  return (
+    <ol id="game-board">
+      {board.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button
+                  onClick={() => onSelectSquare(rowIndex, colIndex)}
+                  disabled={playerSymbol !== null}
+                >
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+};
+
+export default GameBoad;
